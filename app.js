b8a794909e71410004cc4190c22b940e25d17286
p-lacerda <xcloudgameemail@gmail.com>
2022-01-17 21:25:59 -0300

    Faz a requisiçao do tipo POST de /receitas

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.get('/', (_req, res) => {
   res.status(200).send();
 })
 
+app.post('/receitas', (req, res) => {
+  const { descricao, valor, data } = req.body;
+
+  if (!descricao || !valor || !data) {
+    return res.status(400).send('Está faltando alguma das informações')
+  };
+
+  res.status(201).json();
+})
+
 app.listen(PORT, () => {
   console.log('Aplicação está online')
 })
